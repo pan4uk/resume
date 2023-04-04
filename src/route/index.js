67c93fafd156180609cef9d5c7ab6b8eea03fb56
Volodymyr Panchuk <baumana6@gmail.com>
2023-04-04 03:56:24 +0300

    branch car

--- src/route/index.js
+++ src/route/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 
 const router = express.Router()
 
-//=================================================================
-
 var header = {
   name: {
     firstname: 'Dmytro',
@@ -32,13 +30,9 @@ var footer = {
   },
 }
 
-// ================================================================
-
 router.get('/', function (req, res) {
   res.render('index', {})
 })
-
-// ================================================================
 
 router.get('/summary', function (req, res) {
   res.render('summary', {
@@ -67,8 +61,6 @@ router.get('/summary', function (req, res) {
     footer,
   })
 })
-
-// ================================================================
 
 router.get('/skills', function (req, res) {
   res.render('skills', {
@@ -132,8 +124,6 @@ router.get('/skills', function (req, res) {
     footer,
   })
 })
-
-// ================================================================
 
 router.get('/education', function (req, res) {
   res.render('education', {
@@ -751,7 +741,107 @@ router.get('/js', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
-// ================================================================
+router.get('/car', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('car', {
+    page: {
+      title: 'Practice | Car',
+    },
+
+    layout: 'basic',
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2022,
+    color: 'silver',
+    features: {
+      interior: {
+        seats: {
+          material: 'leather',
+          color: 'black',
+          heated: true,
+          ventilated: true,
+        },
+        dashboard: {
+          material: 'plastic',
+          color: 'black',
+          display: {
+            type: 'LCD',
+            size: 10.1,
+            resolution: '1280x720',
+            touchscreen: true,
+          },
+        },
+        audio: {
+          system: 'JBL',
+          speakers: 8,
+          subwoofer: true,
+          bluetooth: true,
+          USB: true,
+        },
+      },
+      exterior: {
+        wheels: {
+          size: 18,
+          type: 'alloy',
+          color: 'silver',
+        },
+        headlights: {
+          type: 'LED',
+          brightness: 'high',
+          automatic: true,
+        },
+        sunroof: {
+          type: 'panoramic',
+          size: 'large',
+          automatic: true,
+        },
+      },
+      safety: {
+        airbags: {
+          front: 2,
+          side: 2,
+          knee: 2,
+          rear: 2,
+        },
+        assistance: {
+          blind_spot_monitoring: true,
+          rear_cross_traffic_alert: true,
+          lane_departure_warning: true,
+          adaptive_cruise_control: true,
+          collision_warning: true,
+        },
+      },
+    },
+    engine: {
+      type: 'gasoline',
+      displacement: 2.5,
+      horsepower: 206,
+      torque: 186,
+      transmission: {
+        type: 'automatic',
+        gears: 8,
+      },
+    },
+    fuel_economy: {
+      city: 28,
+      highway: 39,
+      combined: 32,
+    },
+    price: {
+      base: 25900,
+      destination: 995,
+      options: {
+        navigation: 1200,
+        moonroof: 800,
+        premium_paint: 595,
+      },
+      total: 28990,
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 
 // ================================================================
 
